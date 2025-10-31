3044134edeff2c8caf640d437d6e87b6db54fd3a
Razaool <razaoolhaque@gmail.com>
2025-10-31 23:44:34 +0000

    Complete cleanup of ML match prediction implementation
    Removed all ML-related code and files:
    - Deleted MatchPredictionBox component and CSS
    - Restored API server to pre-ML state
    - Reverted box-8 CSS to original empty container
    - Deleted ML model file (logistic_surface_model.json)
    - Deleted training script (train_logistic_surface.py)
    - Deleted all ML documentation files
    
    Ready for fresh ML implementation from scratch.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ import WinStreakBox from './components/WinStreakBox';
 import HighestELOBySurfaceBox from './components/HighestELOBySurfaceBox';
 import HeadToHeadBox from './components/HeadToHeadBox';
 import RecentMatchesBox from './components/RecentMatchesBox';
-import MatchPredictionBox from './components/MatchPredictionBox';
 import './App.css';
 
 // Types
@@ -72,8 +71,10 @@ function App() {
         {/* Box 9 - Right side container */}
         <HeadToHeadBox className="box-9" />
 
-            {/* Box 8 - Match Prediction */}
-            <MatchPredictionBox className="box-8" />
+            {/* Box 8 - Empty Container */}
+            <div className="box-8">
+              <div className="container-number">8</div>
+            </div>
       </main>
     </div>
   );
